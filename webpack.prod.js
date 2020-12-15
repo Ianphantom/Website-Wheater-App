25e6cc9edddedef3c218e1ccc40d2dfabb4a6ed5
unknown <ian25yola@gmail.com>
2020-12-15 08:19:07 +0700

    webpack untuk tahap production

---
+++ webpack.prod.js
@@ -0,0 +1,34 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.common");
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+ 
+module.exports = merge(common, {
+   mode: "production",
+   module: {
+       rules: [
+           {
+               test: /\.js$/,
+               exclude: "/node_modules/",
+               use: [
+                   {
+                       loader: "babel-loader",
+                       options: {
+                           presets: ["@babel/preset-env"]
+                       }
+                   }
+               ]
+           }
+       ]
+   },
+   plugins: [
+    new CopyWebpackPlugin({
+        patterns: [
+            {
+                from: path.resolve(__dirname, 'assets'),
+                to: path.resolve(__dirname, 'dist/assets')
+            }
+        ]
+    })
+    ]
+})
